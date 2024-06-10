de2b9e9c17b83e388f0f7972aeb5be4776837909
Alexandru Trifon <alexandrut@softinfo.ro>
2024-06-10 10:24:06 +0300

    renunt la koa si fac in plain node

--- restaurantHandler.js
+++
@@ -1,8 +0,0 @@
-var helloWorldHandler = {
-    getHelloWorld: async function (ctx, next) {
-        ctx.body = {response: 'Hello World'};
-        ctx.status = 200;
-        await next();
-    }
-};
-module.exports = helloWorldHandler;
